c64b7e504b172dc988b9ee05c8f6c3d19ffa942a
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-12-30 18:44:32 +0600

    Create modulus&pip.py

---
+++ 16_modulus&pip/modulus&pip.py
@@ -0,0 +1,10 @@
+#math,date and time
+import math
+num=2
+num2=3
+print(math.sqrt(num))
+print(math.pow(num,num2))
+#it work math releted function.....................sin,cos,tan every thing
+import datetime
+x=datetime.datetime.now()
+print(x)
