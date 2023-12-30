71bbf49ec437922799489455938edd154f686d78
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-12-30 18:16:28 +0600

    working Varriable&datatype

---
+++ 2_Varriable&Datatype/Varriable&datatype.py
@@ -0,0 +1,41 @@
+#..................................#Chapter-02;Varrable and Datatype..............................#
+
+
+
+
+# .......................................................Right way.................................>
+
+
+name="RAKIBUL HASAN AKASH"
+fruits="Mango"
+print(fruits)
+print(name)
+phone_number="01885436652"
+print(phone_number)
+
+#....................................................................................................................................#
+
+#.................................................  Wrong way..................................>
+
+# $name="RAKIBUL HASAN AKASH" (!~#$%&^*)
+# frui ts="Mango"(No space)
+# phone-number="01885436652"(-)
+#1id=5688(starting with number)12345678910
+
+
+
+# Data type..............1.number 2.String 3.Booleans 4.List 5.tupple 6.set 7.Dictionary
+#1.NUMBER=1.integer(10)2.float(10.5)
+number=10 #integer
+print(number)
+number2=10.5 #float
+print(number2)
+#boolein
+b=True
+print(b)
+f=False
+print(f)
+
+
+
+
