3283e32e7577dda6acdd35292ca69a087ed9438d
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-12-30 18:35:03 +0600

    while~loop in python

---
+++ 12_While~loop/whileloop.py
@@ -0,0 +1,14 @@
+#Chepter.......12{while loop}
+#syntex
+#initialization
+i=0
+while(i<10): #condition
+    print("Hello_Wrold")
+    i=i+1  #update
+#sum of Nth number:
+sum=0
+i=0
+while(i<=5):
+    sum=sum+i
+    i=i+1
+print(sum)
