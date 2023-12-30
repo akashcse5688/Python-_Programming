b8fd144289e6b9aede58a49ff8bfc5b1fcb9066a
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-12-30 18:20:57 +0600

    number in python

---
+++ 4_Number/number.py
@@ -0,0 +1,35 @@
+#Chepter-4....Working with number of python.................
+#integer,float
+#                 #<...............Geting the type of numerical value.....................>
+number_1=12
+number_2=12.5
+name="RAKIBUL HASAN AKASH"
+li=[1,3,5,6,8]
+tu=("a","b","c")
+print(type(number_1))
+print(type(number_2))
+print(type(name))
+print(type(li))
+print(type(tu))
+# ................................python number method.....................................>
+
+
+# ...Rounding of a number..........................................>
+pi=3.14151926
+print(pi)
+#syntex...round(number,ndights)
+print(round(pi,2))
+
+#....Rasing number of a power.......................................>
+#Syntex-pow(base,exp)
+print(pow(4,3))
+
+#Getting Absolute value(non negative number)..................................>
+#syntex-abs(number)
+a=-100
+print(abs(a))
+
+#Getting quatient and reminder.....................................>
+#Syntex-divmod(number)
+x=divmod(10,3)
+print(x)
