140c5124446fa8eef45834cda0d59a261f96ee3b
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2023-12-30 18:24:25 +0600

    input in python

---
+++ 6_Input/input.py
@@ -0,0 +1,11 @@
+#.......user input from........user........>
+name=input("Enter your name= ")
+print("welcome to = ",name)
+
+
+
+#integer,float
+number1=int(input("Enter your first number= "))
+number2=int(input("Enter your second number= "))
+sum=number1+number2
+print("Sum is = ",sum)
