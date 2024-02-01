a4ff3a17f397857154581c5464276fa804d07521
RAKIBUL HASAN AKASH <123166352+akashcse5688@users.noreply.github.com>
2024-02-01 16:54:27 +0600

    Create Add_Two_Number.py

---
+++ 18_Problem_Sloving/Add_Two_Number.py
@@ -0,0 +1,7 @@
+try:
+    number1=int(input("Enter the first number ="))
+    number2=int(input("Enter the second number"))
+    sum=number1+number2
+    print(f"The sum of number is={number1}+{number2}={sum}")
+except:
+    print("Invalid input entered")
